1e360789b760e8af9e1e43e4ebba84b8a596ce9d
ejunhok <ejunhok@gmail.com>
2023-12-13 22:53:21 -0300

    initial commit

---
+++ src/main/java/io/github/junhokama/microsservice/ScheduleMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.junhokama.microsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleMicrosserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/junhokama/microsservice/ScheduleMicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.junhokama.microsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleMicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
