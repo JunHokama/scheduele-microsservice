acc325e02f7b93de1c69d89eca52e542b46ab062
ejunhok <ejunhok@gmail.com>
2023-12-15 01:41:45 -0300

    feat: adding BusinessException

---
+++ src/main/java/io/github/junhokama/microsservice/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package io.github.junhokama.microsservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message){
+        super(message);
+    }
+}
